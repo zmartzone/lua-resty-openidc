9823c132c3134c3dfb761f1717a41f32163ca32c
Hans Zandbelt <hzandbelt@pingidentity.com>
2015-07-14 19:07:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,173 @@
+# lua-resty-openidc
+
+**lua-resty-openidc** is a library for [NGINX](http://nginx.org/) implementing the
+[OpenID Connect] (http://openid.net/specs/openid-connect-core-1_0.html) Relying Party (RP)
+and the [OAuth 2.0](https://tools.ietf.org/html/rfc6749) Resource Server (RS) functionality.
+
+When used as an OpenID Connect Relying Party it authenticates users against an OpenID Connect
+Provider using [OpenID Connect Discovery](http://openid.net/specs/openid-connect-discovery-1_0.html)
+and the Basic Client Profile (i.e. the Authorization Code flow). When used as an OAuth 2.0
+Resource Server it can validate OAuth 2.0 Bearer Access Tokens against an Authorization Server.
+
+It maintains client-side sessions leveraging `lua-resty-session` so the all of the session state
+for (OpenID Connect0 authenticated users is kept in in a browser cookie, no server-side storage is
+required. Other session management implementations may be added later as lua-resty-session adds
+support for them.
+
+It supports server-wide caching of resolved Discovery documents and validated Access Tokens.
+
+## Dependencies
+
+**lua-resty-openidc** depends on the following packages:
+
+- [NGINX](http://nginx.org/) and [`ngx_devel_kit`](https://github.com/simpl/ngx_devel_kit)
+- [Lua](http://www.lua.org/) or [LuaJIT](http://luajit.org/luajit.html)
+- [`lua-nginx-module`](https://github.com/openresty/lua-nginx-module)
+- [`lua-cjson`](http://www.kyne.com.au/~mark/software/lua-cjson.php)
+- [`lua-resty-string`](https://github.com/openresty/lua-resty-string)
+
+The dependencies above come automatically with [OpenResty](http://openresty.org/). You will need
+to install two extra pure-Lua dependencies that implement session management and http client functions:
+
+- [`lua-resty-http`](https://github.com/pintsized/lua-resty-http)
+- [`lua-resty-session`](https://github.com/bungle/lua-resty-session)
+
+
+## Installation
+
+Copy `openidc.lua` somewhere in your `lua_package_path` under a directory named `resty`.
+If you are using [OpenResty](http://openresty.org/), the default location would be `/usr/local/openresty/lualib/resty`.
+
+
+## Sample Configuration for Google+ Signin
+
+Sample `nginx.conf` configuration for authenticating users against Google+ Signin, protecting a reverse-proxied path.
+
+```
+events {
+  worker_connections 128;
+}
+
+http {
+
+  lua_package_path '~/lua/?.lua;;';
+
+  resolver 8.8.8.8;
+  
+  lua_ssl_trusted_certificate /opt/local/etc/openssl/cert.pem;
+  lua_ssl_verify_depth 5;
+  
+  # cache for discovery metadata documents
+  lua_shared_dict discovery 1m;
+ 
+  server {
+    listen 8080;
+
+    location / {
+
+      access_by_lua '
+
+          local opts = {
+             -- the full redirect URI must be protected by this script and becomes:
+             -- ngx.var.scheme.."://"..ngx.var.http_host..opts.redirect_uri_path
+             redirect_uri_path = "/redirect_uri",
+             discovery = "https://accounts.google.com/.well-known/openid-configuration",
+             client_id = "<client_id",
+             client_secret = "<client_secret"
+             --authorization_params = { hd="pingidentity.com" }
+             --scope = "openid email profile",
+          }
+
+          -- call authenticate for OpenID Connect user authentication
+          local res, err = require("resty.openidc").authenticate(opts)
+          
+          if err then
+            ngx.status = 500
+            ngx.say(err)
+            ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+          end
+         
+          --if res.id_token.hd ~= "pingidentity.com" then
+          --  ngx.exit(ngx.HTTP_FORBIDDEN)
+          --end
+
+          --if res.user.email ~= "hzandbelt@pingidentity.com" then
+          --  ngx.exit(ngx.HTTP_FORBIDDEN)
+          --end
+
+          -- set headers with user info (overwriting any existing!)
+          ngx.req.set_header("X-USER", res.id_token.sub)                    
+      ';
+
+      proxy_pass http://localhost:80;
+    }
+  }
+}
+```
+
+## Sample Configuration for PingFederate OAuth 2.0
+
+Sample `nginx.conf` configuration for validating Bearer Access Tokens against a PingFederate OAuth 2.0 Authorization Server.
+
+```
+events {
+  worker_connections 128;
+}
+
+http {
+
+  lua_package_path '~/lua/?.lua;;';
+
+  resolver 8.8.8.8;
+  
+  lua_ssl_trusted_certificate /opt/local/etc/openssl/cert.pem;
+  lua_ssl_verify_depth 5;
+  
+  # cache for validation results
+  lua_shared_dict introspection 10m;
+ 
+  server {
+    listen 8080;
+
+    location /api {
+
+      access_by_lua '
+ 
+          local opts = {
+             introspection_endpoint="https://localhost:9031/as/token.oauth2",
+             introspection_token_param_name="token",
+             introspection_params = {
+				grant_type="urn:pingidentity.com:oauth2:grant_type:validate_bearer",
+             },
+             client_id="rs_client",
+             client_secret="2Federate"
+          }
+
+          -- call introspect for OAuth 2.0 Bearer Access Token validation
+          local res, err = require("resty.openidc").introspect(opts)
+          
+          if err then
+            ngx.status = 403
+            ngx.say(err)
+            ngx.exit(ngx.HTTP_FORBIDDEN)
+          end
+
+          --if res.scope ~= "edit" then
+          --  ngx.exit(ngx.HTTP_FORBIDDEN)
+          --end
+
+          --if res.client_id ~= "ro_client" then
+          --  ngx.exit(ngx.HTTP_FORBIDDEN)
+          --end          
+      ';
+    }
+  }
+}
+```
+
+## Disclaimer
+
+*This software is open sourced by Ping Identity but not supported commercially
+as such. Any questions/issues should go to the Github issues
+tracker or the author [hzandbelt@pingidentity.com](mailto:hzandbelt@pingidentity.com)
+directly See also the DISCLAIMER file in this directory.*
---
+++ lib/resty/openidc.lua
@@ -0,0 +1,421 @@
+--[[
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+
+***************************************************************************
+Copyright (C) 2015 Ping Identity Corporation
+All rights reserved.
+
+For further information please contact:
+
+     Ping Identity Corporation
+     1099 18th St Suite 2950
+     Denver, CO 80202
+     303.468.2900
+     http://www.pingidentity.com
+
+DISCLAIMER OF WARRANTIES:
+
+THE SOFTWARE PROVIDED HEREUNDER IS PROVIDED ON AN "AS IS" BASIS, WITHOUT
+ANY WARRANTIES OR REPRESENTATIONS EXPRESS, IMPLIED OR STATUTORY; INCLUDING,
+WITHOUT LIMITATION, WARRANTIES OF QUALITY, PERFORMANCE, NONINFRINGEMENT,
+MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  NOR ARE THERE ANY
+WARRANTIES CREATED BY A COURSE OR DEALING, COURSE OF PERFORMANCE OR TRADE
+USAGE.  FURTHERMORE, THERE ARE NO WARRANTIES THAT THE SOFTWARE WILL MEET
+YOUR NEEDS OR BE FREE FROM ERRORS, OR THAT THE OPERATION OF THE SOFTWARE
+WILL BE UNINTERRUPTED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+@Author: Hans Zandbelt - hzandbelt@pingidentity.com
+--]]
+
+local cjson = require "cjson"
+local http  = require "resty.http"
+
+local openidc = {
+  _VERSION = "1.0"
+}
+openidc.__index = openidc
+
+-- set value in server-wide cache if available
+local function openidc_cache_set(type, key, value, exp)
+  local dict = ngx.shared[type]
+  if dict then
+    local success, err, forcible = dict:set(key, value, exp)
+    ngx.log(ngx.DEBUG, "cache set: success=", success, " err=", err, " forcible=", forcible)
+  end
+end
+
+-- retrieve value from server-wide cache if available
+local function openidc_cache_get(type, key)
+  local dict = ngx.shared[type]
+  local value = nil
+  local flags = nil
+  if dict then
+    value, flags = dict:get(key)
+    if value then ngx.log(ngx.DEBUG, "cache hit: type=", type, " key=", key) end
+  end
+  return value
+end
+
+-- validate the contents of and id_token
+local function openidc_validate_id_token(opts, id_token)
+
+  -- check issuer
+  if opts.discovery.issuer ~= id_token.iss then
+    ngx.log(ngx.ERR, "issuer \"", id_token.iss, " in id_token is not equal to the issuer from the discovery document \"", opts.discovery.issuer, "\"")
+    return false
+  end
+
+  -- check issued-at timestamp
+  if id_token.iat < (os.time() - 10) then
+    ngx.log(ngx.ERR, "token is not valid yet: id_token.iat=", id_token.iat, ", os.time()=", os.time())
+    return false
+  end
+
+  -- check expiry timestamp
+  if id_token.exp < os.time() then
+    ngx.log(ngx.ERR, "token expired: id_token.exp=", id_token.exp, ", os.time()=", os.time())
+    return false
+  end
+
+  -- check audience (array or string)
+  if (type(id_token.aud) == "table") then
+    for key, value in pairs(id_token.aud) do
+      if value == opts.client_id then
+        return true
+      end
+    end
+    ngx.log(ngx.ERR, "no match found token audience array: client_id=", opts.client_id )
+    return false
+  elseif  (type(id_token.aud) == "string") then
+    if id_token.aud ~= opts.client_id then
+      ngx.log(ngx.ERR, "token audience does not match: id_token.aud=", id_token.aud, ", client_id=", opts.client_id )
+      return false
+    end
+  end
+  return true
+end
+
+-- assemble the redirect_uri
+local function openidc_get_redirect_uri(opts)
+  return ngx.var.scheme.."://"..ngx.var.http_host..opts.redirect_uri_path
+end
+
+-- perform base64url decoding
+local function openidc_base64_url_decode(input)
+  local reminder = #input % 4
+  if reminder > 0 then
+    local padlen = 4 - reminder
+    input = input .. string.rep('=', padlen)
+  end
+  input = input:gsub('-','+'):gsub('_','/')
+  return ngx.decode_base64(input)
+end
+
+-- send the browser of to the OP's authorization endpoint
+local function openidc_authorize(opts, session)
+  local resty_random = require "resty.random"
+  local resty_string = require "resty.string"
+
+  -- generate state
+  local state = resty_string.to_hex(resty_random.bytes(16))
+
+  -- assemble the parameters to the authentication request
+  local params = {
+    client_id=opts.client_id,
+    response_type="code",
+    scope=opts.scope and opts.scope or "openid email profile",
+    redirect_uri=openidc_get_redirect_uri(opts),
+    state=state
+  }
+
+  -- merge any provided extra parameters
+  if opts.authorization_params then
+    for k,v in pairs(opts.authorization_params) do params[k] = v end
+  end
+
+  -- store state in the session
+  session.data.original_url = ngx.var.uri
+  session.data.state = state
+  session:save()
+
+  -- redirect to the /authorization endpoint
+  return ngx.redirect(opts.discovery.authorization_endpoint.."?"..ngx.encode_args(params))
+end
+
+-- parse the JSON result from a call to the OP
+local function openidc_parse_json_response(response)
+
+  local err = nil
+  local res = nil
+
+  -- check the response from the OP
+  if response.status ~= 200 then
+    err = "response indicates failure, status="..response.status..", body="..response.body
+  else
+    -- decode the response and extract the JSON object
+    res = cjson.decode(response.body)
+
+    if not res then
+      err = "JSON decoding failed"
+    end
+  end
+
+  return res, err
+end
+
+-- make a call to the token endpoint
+local function openidc_call_token_endpoint(endpoint, body)
+
+  ngx.log(ngx.DEBUG, "request body for token endpoint call: ", ngx.encode_args(body))
+
+  local httpc = http.new()
+  local res, err = httpc:request_uri(endpoint, {
+    method = "POST",
+    body = ngx.encode_args(body),
+    headers = {
+      ["Content-Type"] = "application/x-www-form-urlencoded",
+    }
+  })
+  if not res then
+    err = "accessing token endpoint ("..endpoint..") failed: "..err
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  ngx.log(ngx.DEBUG, "token endpoint response: ", res.body)
+
+  return openidc_parse_json_response(res);
+end
+
+-- make a call to the userinfo endpoint
+local function openidc_call_userinfo_endpoint(opts, access_token)
+
+  local httpc = http.new()
+  local res, err = httpc:request_uri(opts.discovery.userinfo_endpoint, {
+    headers = {
+      ["Authorization"] = "Bearer "..access_token,
+    }
+  })
+  if not res then
+    err = "accessing userinfo endpoint ("..opts.discovery.userinfo_endpoint..") failed: "..err
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  ngx.log(ngx.DEBUG, "userinfo response: ", res.body)
+
+  -- parse the response from the user info endpoint
+  return openidc_parse_json_response(res)
+end
+
+-- handle a "code" authorization response from the OP
+local function openidc_authorization_response(opts, session)
+  local args = ngx.req.get_uri_args()
+  local err = nil
+
+  if not args.code or not args.state then
+    err = "unhandled request to the redirect_uri: "..ngx.var.request_uri
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  -- check that the state returned in the response against the session; prevents CSRF
+  if args.state ~= session.data.state then
+    err = "state from argument: "..args.state.." does not match state restored from session: "..session.data.state
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  -- assemble the parameters to the token endpoint
+  local body = {
+    grant_type="authorization_code",
+    client_id=opts.client_id,
+    client_secret=opts.client_secret,
+    code=args.code,
+    redirect_uri=openidc_get_redirect_uri(opts)
+  }
+
+  -- make the call to the token endpoint
+  local json, err = openidc_call_token_endpoint(opts.discovery.token_endpoint, body)
+  if err then
+    return nil, err
+  end
+
+  -- process the token endpoint response with the id_token and access_token
+  local enc_hdr, enc_pay, enc_sign = string.match(json.id_token, '^(.+)%.(.+)%.(.+)$')
+  local jwt = openidc_base64_url_decode(enc_pay)
+  session.data.id_token = cjson.decode(jwt)
+  session.data.access_token = json.access_token
+
+  -- validate the id_token contents
+  if openidc_validate_id_token(opts, session.data.id_token) == false then
+    err = "id_token validation failed"
+    return nil, err
+  end
+
+  -- call the user info endpoint
+  session.data.user, err = openidc_call_userinfo_endpoint(opts, json.access_token)
+
+  -- save the session with the obtained id_token
+  session:save()
+
+  -- redirect to the URL that was accessed originally
+  return ngx.redirect(session.data.original_url)
+
+end
+
+-- get the Discovery metadata from the specified URL
+local function openidc_discover(url)
+
+  local err = nil
+  local v = openidc_cache_get("discovery", url)
+  if not v then
+
+    -- make the call to the discovery endpoint
+    local httpc = http.new()
+    local res, error = httpc:request_uri(url)
+    if not res then
+      err = "accessing discovery url ("..url..") failed: "..error
+      ngx.log(ngx.ERR, err)
+    else
+      json, err = openidc_parse_json_response(res)
+      if json then
+        openidc_cache_set("discovery", url, cjson.encode(json), 24 * 60 * 60)
+      end
+    end
+
+  else
+    json = cjson.decode(v)
+  end
+
+  return json, err
+end
+
+-- main routine for OpenID Connect user authentication
+function openidc.authenticate(opts)
+
+  local err = nil
+
+  local session = require("resty.session").start()
+
+  if type(opts.discovery) == "string" then
+    opts.discovery, err = openidc_discover(opts.discovery)
+    if err then
+      return nil, err
+    end
+  end
+
+  -- see if this is a request to the redirect_uri i.e. an authorization response
+  local path = ngx.var.request_uri
+  path = path:match("(.-)%?") or path
+  if path == opts.redirect_uri_path then
+    return openidc_authorization_response(opts, session)
+  end
+
+  -- if we have no id_token then redirect to the OP for authentication
+  if not session.data.id_token then
+    return openidc_authorize(opts, session)
+  end
+
+  -- log id_token contents
+  ngx.log(ngx.DEBUG, "id_token=", cjson.encode(session.data.id_token))
+
+  -- return the id_token to the caller Lua script for access control purposes
+  return
+    {
+      id_token=session.data.id_token,
+      access_token=session.data.access_token,
+      user=session.data.user
+    },
+    err
+end
+
+-- main routine for OAuth 2.0 token introspection
+function openidc.introspect(opts)
+
+  local err = nil
+
+  -- get the access token from the Authorization header
+  local headers = ngx.req.get_headers()
+  local header =  headers['Authorization']
+
+  if header == nil or header:find(" ") == nil then
+    err = "no Authorization header found"
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  local divider = header:find(' ')
+  if string.lower(header:sub(0, divider-1)) ~= string.lower("Bearer") then
+    err = "no Bearer authorization header value found"
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  local access_token = header:sub(divider+1)
+  if access_token == nil then
+    err = "no Bearer access token value found"
+    ngx.log(ngx.ERR, err)
+    return nil, err
+  end
+
+  -- see if we've previously cached the introspection result for this access token
+  local json = nil
+  local v = openidc_cache_get("introspection", access_token)
+  if not v then
+
+    -- assemble the parameters to the introspection (token) endpoint
+    local token_param_name = opts.introspection_token_param_name and opts.introspection_token_param_name or "access_token"
+
+    local body = {}
+
+    body[token_param_name]= access_token
+
+    if opts.client_id then
+      body.client_id=opts.client_id
+    end
+    if opts.client_secret then
+      body.client_secret=opts.client_secret
+    end
+
+    -- merge any provided extra parameters
+    if opts.introspection_params then
+      for k,v in pairs(opts.introspection_params) do body[k] = v end
+    end
+
+    -- call the introspection endpoint
+    json, err = openidc_call_token_endpoint(opts.introspection_endpoint, body)
+
+    -- cache the results
+    if json then
+      openidc_cache_set("introspection", access_token, cjson.encode(json), json.expires_in)
+    end
+
+  else
+    json = cjson.decode(v)
+  end
+
+  return json, err
+end
+
+return openidc
