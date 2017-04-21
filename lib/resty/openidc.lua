347e6698ad5666e7d3ee418f546ff92a2cb117b9
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-04-21 15:07:36 -0700

    Always return the session object from openidc.authenticate() as this is needed for the caller, if it wants to modify session data with lua-resty-session

--- lib/resty/openidc.lua
+++ lib/resty/openidc.lua
@@ -284,28 +284,28 @@ local function openidc_authorization_response(opts, session)
   if not args.code or not args.state then
     err = "unhandled request to the redirect_uri: "..ngx.var.request_uri
     ngx.log(ngx.ERR, err)
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- check that the state returned in the response against the session; prevents CSRF
   if args.state ~= session.data.state then
     err = "state from argument: "..(args.state and args.state or "nil").." does not match state restored from session: "..(session.data.state and session.data.state or "nil")
     ngx.log(ngx.ERR, err)
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- check the iss if returned from the OP
   if args.iss and args.iss ~= opts.discovery.issuer then
     err = "iss from argument: "..args.iss.." does not match expected issuer: "..opts.discovery.issuer
     ngx.log(ngx.ERR, err)
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- check the client_id if returned from the OP
   if args.client_id and args.client_id ~= opts.client_id then
     err = "client_id from argument: "..args.client_id.." does not match expected client_id: "..opts.client_id
     ngx.log(ngx.ERR, err)
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- assemble the parameters to the token endpoint
@@ -319,7 +319,7 @@ local function openidc_authorization_response(opts, session)
   -- make the call to the token endpoint
   local json, err = openidc_call_token_endpoint(opts, opts.discovery.token_endpoint, body, opts.token_endpoint_auth_method)
   if err then
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- process the token endpoint response with the id_token and access_token
@@ -330,7 +330,7 @@ local function openidc_authorization_response(opts, session)
   -- validate the id_token contents
   if openidc_validate_id_token(opts, id_token, session.data.nonce) == false then
     err = "id_token validation failed"
-    return nil, err, session.data.original_url
+    return nil, err, session.data.original_url, session
   end
 
   -- call the user info endpoint
@@ -347,7 +347,7 @@ local function openidc_authorization_response(opts, session)
   session:save()
 
   -- redirect to the URL that was accessed originally
-  return ngx.redirect(session.data.original_url)
+  return ngx.redirect(session.data.original_url), session
 
 end
 
@@ -546,7 +546,7 @@ function openidc.authenticate(opts, target_url)
     --end
     opts.discovery, err = openidc_discover(opts.discovery, opts.ssl_verify)
     if err then
-      return nil, err, target_url
+      return nil, err, target_url, session
     end
   end
 
@@ -559,19 +559,19 @@ function openidc.authenticate(opts, target_url)
     if not session.present then
       err = "request to the redirect_uri_path but there's no session state found"
       ngx.log(ngx.ERR, err)
-      return nil, err, target_url
+      return nil, err, target_url, session
     end
-    return openidc_authorization_response(opts, session)
+    return openidc_authorization_response(opts, session), session
   end
 
   -- see if this is a request to logout
   if path == (opts.logout_path and opts.logout_path or "/logout") then
-    return openidc_logout(opts, session)
+    return openidc_logout(opts, session), session
   end
 
   -- if we have no id_token then redirect to the OP for authentication
   if not session.present or not session.data.id_token then
-    return openidc_authorize(opts, session, target_url)
+    return openidc_authorize(opts, session, target_url), session
   end
 
   -- log id_token contents
@@ -585,7 +585,8 @@ function openidc.authenticate(opts, target_url)
       user=session.data.user
     },
     err,
-    target_url
+    target_url,
+    session
 end
 
 -- get an OAuth 2.0 bearer access token from the HTTP request
