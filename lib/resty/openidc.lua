7202bcf568b9a270c9bdd0096e4d773dbbd26991
Peter <peter.coppens@datylon.com>
2017-07-03 18:58:46 +0200

    Added boolean config property to optionally add id_token_hint on redirect after logout (default = false)

--- lib/resty/openidc.lua
+++ lib/resty/openidc.lua
@@ -495,8 +495,10 @@ local function openidc_logout(opts, session)
     ngx.print(openidc_transparent_pixel)
     ngx.exit(ngx.OK)
     return
-  elseif opts.redirect_after_logout_uri then
+  elseif opts.redirect_after_logout_uri and redirect_after_logout_with_id_token_hint then
     return ngx.redirect(opts.redirect_after_logout_uri.."&id_token_hint="..session_token)
+  elseif opts.redirect_after_logout_uri then
+    return ngx.redirect(opts.redirect_after_logout_uri)
   elseif opts.discovery.end_session_endpoint then
     return ngx.redirect(opts.discovery.end_session_endpoint)
   elseif opts.discovery.ping_end_session_endpoint then
